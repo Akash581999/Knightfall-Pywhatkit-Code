63bc4de4339908e73c01b182a464856f384e12ac
Akash581999 <akash581999@gmail.com>
2024-07-21 10:46:42 +0530

    first commit

---
+++ Codeblack.py
@@ -0,0 +1,5 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 76176 10707','Hello there!',11,00)
+# pywhatkit.sendwhatmsg('+91 89581 40626','This is Codeblack ......Initiate Knightfall protocol',22,30)
+# pywhatkit.sendwhatmsg('+91 60064 72058','This is Codeblack ......Initiate Knightfall protocol',22,30)
+# pywhatkit.sendwhatmsg('+91 96394 50760','This is Codeblack ......Initiate Knightfall protocol',22,30)
